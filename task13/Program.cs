f6aa4f5a592e41280522b40cdb9e78f0ea76b0a9
Abashin Aleksey <mmilasomm@gmail.com>
2022-09-20 19:56:03 +0300

    задача №13

---
+++ task13/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру 
+// заданного числа или сообщает, что третьей цифры нет.
+Console.Clear();
+Console.WriteLine(" введите трёхзначное число");
+int num = int.Parse(Console.ReadLine()!);
+
+if (num < 100)
+{
+    Console.WriteLine("третьего числа нет ! ");
+}
+else
+{
+    num = num % 10 ;
+    Console.WriteLine($"третья цифра = {num}");
+}
