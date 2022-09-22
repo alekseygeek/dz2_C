64cc00891690ea643b9a483b06c1fb4afc2df690
Abashin Aleksey <mmilasomm@gmail.com>
2022-09-22 22:50:47 +0300

    лекция 3

--- test/Program.cs
+++ test/Program.cs
@@ -1,4 +1,84 @@
 ﻿// test
-Console.Write("число");
-int age = Convert.ToInt32(Console.ReadLine());
+//Console.Write("число");
+//int age = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+//int[] array = { 23, 34, 45, 56, 6, 7, 23, 9, 67};
+//
+//int n = array.Length;
+//int find = 1;
+//
+//int index = 0;
+// 
+//while (index < n)
+//{
+//    if(array[index] == find)
+//   {
+//        Console.WriteLine(index);
+//        break;
+//    }
+//    index++;
+// } 
+
+//int[] array = new int[10;]
+
+// 17. Напишите программу, которая принимает на вход координаты точки (X и Y),
+// причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка
+
+// Задача 18: Напишите программу, которая по заданному номеру четверти, показывает 
+// диапазон возможных координат точек в этой четверти (x и y).
+
+// Console.WriteLine("введите четверть ");
+// int a = int.Parse(Console.ReadLine());
+// if(a == 1)
+// {
+//     Console.WriteLine("x > 0 , y > 0");
+// }
+// else if(a == 2)
+// {
+//     Console.WriteLine("x < 0 , y > 0");
+// }
+// else if(a == 3)
+// {
+//     Console.WriteLine("x < 0 , y < 0");
+// }
+// else if(a == 4)
+// {
+//     Console.WriteLine("x > 0 , y < 0");
+// } else{
+//     Console.WriteLine("не верная четверть");
+// }
+// Задача 21: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+//A (3,6); B (2,1) -> 5,09 
+//A (7,-5); B (1,-1) -> 7,21
+
+// Console.WriteLine("Введите x1: ");
+// int x1 = int.Parse(Console.ReadLine());
+
+// Console.WriteLine("Введите y1: ");
+// int y1 = int.Parse(Console.ReadLine());
+
+// Console.WriteLine("Введите x2: ");
+// int x2 = int.Parse(Console.ReadLine());
+
+// Console.WriteLine("Введите y2: ");
+// int y2 = int.Parse(Console.ReadLine());
+
+// double r = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 -  y1, 2));
+
+// Console.WriteLine($"Расстояние равно {Math.Round(r, 2)}");
+
+
+// Задача 22: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+// Console.WriteLine("Введите число: ");
+// int N = int.Parse(Console.ReadLine());
+// int i = 1;
+
+// while(i <= N)
+// {
+//     Console.WriteLine(Math.Pow(i, 2));
+//     i++ ;
+// }
 
