19d5b6491145a4ac19e46233cbf7554d60dde5e5
Abashin Aleksey <mmilasomm@gmail.com>
2022-09-22 10:12:11 +0300

    решили задачу №10

---
+++ task10/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и 
+// на выходе показывает вторую цифру этого числа.
+Console.Clear();
+Console.WriteLine("введите трёхзначное число :");
+int num = Convert.ToInt32(Console.ReadLine());
+int num1 = Math.Abs(num);
+if ((num1 >= 999) || (num1 <= 99))
+{
+    Console.WriteLine("это не трёхзначное число !!!");
+}
+else
+{
+   Console.WriteLine($"второе число трёхзначного числа :{num1 / 10 % 10}"); 
+}
