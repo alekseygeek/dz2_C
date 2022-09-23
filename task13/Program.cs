b16993db811b60c0820406f697b744fe379e1fbc
Abashin Aleksey <mmilasomm@gmail.com>
2022-09-23 13:19:42 +0300

    переписали задачу № 13

--- task13/Program.cs
+++ task13/Program.cs
@@ -1,28 +1,41 @@
 ﻿// Задача 13: Напишите программу, которая выводит третью цифру 
 // заданного числа или сообщает, что третьей цифры нет.
 Console.Clear();
-Console.Write(" введите  число :");
+// Console.Write(" введите  число :");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int num1 = Math.Abs(num);
+// if (num1 < 100 )
+// {
+//     Console.WriteLine("третьего числа нет ! ");
+// }
+// if (num1 <= 999 && num1 >= 100)
+// {
+//     Console.WriteLine(num1 % 10);
+// }
+// if (num1 > 999)
+// {
+//     while(num1 > 999)
+//     {
+//         num1 = num1 / 10;
+//         if(num1 <= 999) 
+//         {
+//             Console.WriteLine(num1 % 10);
+//         }
+
+//     }
+// }
+Console.Write("введите число :");
 int num = Convert.ToInt32(Console.ReadLine());
-int num1 = Math.Abs(num);
-if (num1 < 100 )
+num = Math.Abs(num);
+if (num > 99)
 {
-    Console.WriteLine("третьего числа нет ! ");
-}
-if (num1 <= 999 && num1 >= 100)
-{
-    Console.WriteLine(num1 % 10);
-}
-if (num1 > 999)
-{
-    while(num1 > 999)
+    while(num > 999)
     {
-        num1 = num1 / 10;
-        if(num1 <= 999) 
-        {
-            Console.WriteLine(num1 % 10);
-        }
-
+    num /= 10;
     }
+    Console.WriteLine("третья цифра  :" + num%10);
 }
-
-
+else
+{
+    Console.WriteLine("не третьего числа");
+}
