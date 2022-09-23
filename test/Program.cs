1834a88b1aafa350e407b959619e8fc55fa87032
Abashin Aleksey <mmilasomm@gmail.com>
2022-09-23 19:26:47 +0300

    решили задачу № 23

--- test/Program.cs
+++ test/Program.cs
@@ -134,23 +134,35 @@ Console.Clear();
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
 
-Console.Write("введите координату X первой точки: ");
-int x = int.Parse(Console.ReadLine());
-Console.Write("введите координату Y первой точки: ");
-int y = int.Parse(Console.ReadLine());
-Console.Write("введите координату Z первой точки: ");
-int z = int.Parse(Console.ReadLine());
-Console.Write("введите координату X второй точки: ");
-int x1 = int.Parse(Console.ReadLine());
-Console.Write("введите координату Y второй точки: ");
-int y1 = int.Parse(Console.ReadLine());
-Console.Write("введите координату Z второй точки: ");
-int z1 = int.Parse(Console.ReadLine());
+// Console.Write("введите координату X первой точки: ");
+// int x = int.Parse(Console.ReadLine());
+// Console.Write("введите координату Y первой точки: ");
+// int y = int.Parse(Console.ReadLine());
+// Console.Write("введите координату Z первой точки: ");
+// int z = int.Parse(Console.ReadLine());
+// Console.Write("введите координату X второй точки: ");
+// int x1 = int.Parse(Console.ReadLine());
+// Console.Write("введите координату Y второй точки: ");
+// int y1 = int.Parse(Console.ReadLine());
+// Console.Write("введите координату Z второй точки: ");
+// int z1 = int.Parse(Console.ReadLine());
 
-int A = x - x1;
-int B = y - y1;
-int C = z - z1;
+// int A = x - x1;
+// int B = y - y1;
+// int C = z - z1;
 
-double length = Math.Sqrt(A * A + B * B + C * C);
-length = Math.Round(length, 2);
-Console.WriteLine($"длинна отрезка {length}");
+// double length = Math.Sqrt(A * A + B * B + C * C);
+// length = Math.Round(length, 2);
+// Console.WriteLine($"длинна отрезка {length}");
+
+//Задача 23 Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+
+
+Console.Write(" введите число : ");
+int num = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= num; i++)
+{
+     Console.WriteLine($"{i * i * i} ");
+}
